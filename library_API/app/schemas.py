c737682c97183edf686a1367474e5057ccb1cada
clemens2107 <69518240+Melrydin@users.noreply.github.com>
2025-09-05 09:44:00 +0200

    feat: Introduce BookUpdateSchema for partial updates
    - Add `BookUpdateSchema` with all fields marked as optional.
    - This new schema is specifically designed for `PATCH` and `PUT` requests, ensuring data validation for partial updates.
    - It replaces the need to send all book attributes when only a few need to be changed.

--- library_API/app/schemas.py
+++ library_API/app/schemas.py
@@ -1,33 +1,52 @@
 from sqlmodel import Field
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from datetime import date
-from typing import Optional
+from typing import Optional, List
 from enum import Enum
 
 
 class BookTyps(str, Enum):
-    manga = "Manga"
-    Novel = "Novel"
-    Technical = "Technical"
+	manga = "Manga"
+	Novel = "Novel"
+	Technical = "Technical"
 
 
 class BookDataSchema(BaseModel):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    category: BookTyps
-    title: str
-    series: bool
-    volume: int
-    author: str
-    publisher: str
-    price: float
-    isbn: int
-    wishlist: Optional[bool] = Field(default=True)
-    gift: Optional[bool] = Field(default=False)
-    borrow: Optional[bool] = Field(default=False)
-    releaseDate: Optional[date] = Field(default=None)
-    payDate: Optional[date] = Field(default=None)
-    startDate: Optional[date] = Field(default=None)
-    endDate: Optional[date] = Field(default=None)
+	id: Optional[int] = Field(default=None, primary_key=True)
+	category: BookTyps
+	title: str
+	series: bool
+	volume: int
+	author: str
+	publisher: str
+	price: float
+	isbn: int
+	wishlist: Optional[bool] = Field(default=True)
+	gift: Optional[bool] = Field(default=False)
+	borrow: Optional[bool] = Field(default=False)
+	releaseDate: Optional[date] = Field(default=None)
+	payDate: Optional[date] = Field(default=None)
+	startDate: Optional[date] = Field(default=None)
+	endDate: Optional[date] = Field(default=None)
 
-    class Config:
-        orm_mode = True
+	model_config = ConfigDict(from_attributes=True)
+
+
+# Scheme for updating books
+class BookUpdateSchema(BaseModel):
+	# All fields are optional and have no default value.
+	category: Optional[BookTyps] = None
+	title: Optional[str] = None
+	series: Optional[bool] = None
+	volume: Optional[int] = None
+	author: Optional[str] = None
+	publisher: Optional[str] = None
+	price: Optional[float] = None
+	isbn: Optional[int] = None
+	wishlist: Optional[bool] = None
+	gift: Optional[bool] = None
+	borrow: Optional[bool] = None
+	releaseDate: Optional[date] = None
+	payDate: Optional[date] = None
+	startDate: Optional[date] = None
+	endDate: Optional[date] = None
